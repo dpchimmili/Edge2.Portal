9a65a63f5ecea4108e9e0a41e353cb53656f51e2
dpchimmili <prasad.pro5@gmail.com>
2020-06-03 17:10:41 +0530

    Update server.js
    Modified server.js file

--- server/server.js
+++ server/server.js
@@ -4,8 +4,11 @@ const app = express();
 const publicPath = path.join(__dirname, '..', 'public');
 const port = process.env.PORT || 3000;
 app.use(express.static(publicPath));
-app.get('*', (req, res) => {
-   res.sendFile(path.join(publicPath, 'index.html'));
+app.get("*", (req, res) => {
+  let url = path.join(__dirname, '../client/build', 'index.html');
+  if (!url.startsWith('/app/')) // since we're on local windows
+    url = url.substring(1);
+  res.sendFile(url);
 });
 app.listen(port, () => {
    console.log('Server is up!');

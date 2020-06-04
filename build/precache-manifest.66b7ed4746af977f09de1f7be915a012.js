5e96c76f574c37e0a1d5369cddd348835337767e
Durga Prasad Chimmili <prasad.pro5@gmail.com>
2020-06-04 11:25:42 +0530

    removed node modules from going into github

---
+++ build/precache-manifest.66b7ed4746af977f09de1f7be915a012.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "911e896201c036ceb853d3661ced4821",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c14d47f15c84434c1c9e",
+    "url": "/static/css/2.5b088533.chunk.css"
+  },
+  {
+    "revision": "f529fae26a403c9ab64b",
+    "url": "/static/css/main.0389a127.chunk.css"
+  },
+  {
+    "revision": "c14d47f15c84434c1c9e",
+    "url": "/static/js/2.b1688eea.chunk.js"
+  },
+  {
+    "revision": "c560b744118e776c5a9fe14e0546e12a",
+    "url": "/static/js/2.b1688eea.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f529fae26a403c9ab64b",
+    "url": "/static/js/main.87809a41.chunk.js"
+  },
+  {
+    "revision": "743fcdd735a49de9f0b8",
+    "url": "/static/js/runtime-main.e3b87036.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
